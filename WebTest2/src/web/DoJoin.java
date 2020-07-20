c9688f9a7a2abc02a68a942afd0cd89e5e66b842
jjwon915 <jjwon915@naver.com>
2020-07-20 22:56:48 +0900

    [한글 쑥쑥 회원가입 - 보호자 회원가입]
    - DB Connection을 통해 직접 만든 DB에 이름, 아이디, 비밀번호, 생년월일, 핸드폰 번호를 저장하고 회원가입을 완료하도록 한다.

---
+++ WebTest2/src/web/DoJoin.java
@@ -0,0 +1,69 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DBUtil;
+
+/**
+ * Servlet implementation class DoJoin
+ */
+@WebServlet("/doJoin")
+public class DoJoin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoJoin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String password = request.getParameter("pw");
+		String birth = request.getParameter("birth");
+		String phone = request.getParameter("phone");
+
+		ServletContext sc = getServletContext();
+		Connection conn = (Connection) sc.getAttribute("DBconnection2");
+		
+		try {
+			DBUtil.joinUser(conn, name, id, password, birth, phone);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher view = request.getRequestDispatcher("Home.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
